fadd42beb0db9e9a3d4e01e3312935aa94531a7c
vagrant-homestead <qtk.jack@gmail.com>
2018-09-25 18:19:49 +0000

    finish basic page layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由 <a href="http://wangyapeng.me" target="_blank">隔壁老王</a> 
+            设计和编码 
+            <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="pull-right"><a href="mailto:qtk.jack@gmail.com">联系我</a></p>
+    </div>
+</footer>
