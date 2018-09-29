76ee9208e733e58fb3842b116b2533b491ab2a52
vagrant-homestead <qtk.jack@gmail.com>
2018-09-29 16:29:49 +0000

    admin can manage site settings

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -6,6 +6,6 @@
             <span style="color: #e27575;font-size: 14px;">❤</span>
         </p>
 
-        <p class="pull-right"><a href="mailto:qtk.jack@gmail.com">联系我</a></p>
+        <p class="pull-right"><a href="mailto:{{ setting('contact_email') }}">联系我</a></p>
     </div>
 </footer>
