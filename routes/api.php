c346edce8a99f19890f7edf57ddad49f90a19274
vagrant-homestead <qtk.jack@gmail.com>
2018-10-04 21:43:19 +0000

    replies index

--- routes/api.php
+++ routes/api.php
@@ -79,6 +79,11 @@ $api->version('v1', [
         // 获取话题详情
         $api->get('topics/{topic}', 'TopicsController@show')->name('api.topics.show');
 
+        // 获取某个话题的回复列表
+        $api->get('topics/{topic}/replies', 'RepliesController@index')->name('api.topics.replies.index');
+
+        // 某个用户的回复列表
+        $api->get('users/{user}/replies', 'RepliesController@userIndex')->name('api.users.replies.index');
         
 
         // 需要 token 验证的接口
