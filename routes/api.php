2379ee0cfe94c628445f9d8f1c382b2576eed2f8
vagrant-homestead <qtk.jack@gmail.com>
2018-10-05 09:30:12 +0000

    notifications stats

--- routes/api.php
+++ routes/api.php
@@ -116,7 +116,8 @@ $api->version('v1', [
             // 消息通知列表
             $api->get('user/notifications', 'NotificationsController@index')->name('api.user.notifications.index');
             
-
+            // 未读通知统计
+            $api->get('user/notifications/stats', 'NotificationsController@stats')->name('api.user.notifications.stats');
 
         });
 
